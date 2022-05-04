2653f177f7fce1b2836f5a4a1ba74da222eac605
wilkowsk <104863887+wilkowsk@users.noreply.github.com>
2022-05-03 22:57:33 -0400

    Binary search
    Binary search

--- aifm/test/test_tcp_array_add.cpp
+++ aifm/test/test_tcp_array_add.cpp
@@ -25,8 +25,6 @@ constexpr static uint32_t kNumEntries =
 constexpr static uint32_t kNumConnections = 300;
 
 uint64_t raw_array_A[kNumEntries];
-uint64_t raw_array_B[kNumEntries];
-uint64_t raw_array_C[kNumEntries];
 
 template <uint64_t N, typename T>
 void copy_array(Array<T, N> *array, T *raw_array) {
@@ -46,30 +44,58 @@ void add_array(Array<T, N> *array_C, Array<T, N> *array_A,
   }
 }
 
+template <typename T, uint64_t N>
+int search_array(Array<T, N> *array_A, uint64_t targetElement) {
+  uint64_t low = 0;
+  uint64_t high = N - 1;
+  uint64_t middle;
+  uint64_t middleElement;
+  bool foundElement = false;
+  while ((high >= low) && !foundElement) {
+    DerefScope scope;
+    middle = (high + low) / 2;
+    middleElement = (*array_A).at(scope, middle);
+    if (middleElement > targetElement) { // middleElement is too high
+      high = middle - 1;
+    } elseif (middleElement < targetElement) { // middleElement is too low
+      low = middle + 1;
+    } else {
+      foundElement = true;
+    }
+  }
+  if (foundElement) {
+    return middle;
+  }
+  return -1;
+}
+
 void gen_random_array(uint64_t num_entries, uint64_t *raw_array) {
   std::random_device rd;
   std::mt19937_64 eng(rd());
-  std::uniform_int_distribution<uint64_t> distr;
-
+  std::uniform_int_distribution<uint64_t> distr(2,257);
+  uint64_t value = 0;
   for (uint64_t i = 0; i < num_entries; i++) {
-    raw_array[i] = distr(eng);
+    value += distr(eng);
+    raw_array[i] = value;
   }
 }
 
 void do_work(FarMemManager *manager) {
   auto array_A = manager->allocate_array<uint64_t, kNumEntries>();
-  auto array_B = manager->allocate_array<uint64_t, kNumEntries>();
-  auto array_C = manager->allocate_array<uint64_t, kNumEntries>();
 
   gen_random_array(kNumEntries, raw_array_A);
-  gen_random_array(kNumEntries, raw_array_B);
   copy_array(&array_A, raw_array_A);
-  copy_array(&array_B, raw_array_B);
   add_array(&array_C, &array_A, &array_B);
+  uint64_t result;
 
-  for (uint64_t i = 0; i < kNumEntries; i++) {
+  for (uint64_t i = 0; i < 10000; i += 100) {
     DerefScope scope;
-    if (array_C.at(scope, i) != raw_array_A[i] + raw_array_B[i]) {
+    result = search_array(&array_A, raw_array_A[i]);
+    if (result != i) {
+      goto fail;
+    }
+    result = search_array(&array_A, raw_array_A[i] + 1);
+    if (result != -1) {
       goto fail;
     }
   }
