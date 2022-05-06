c1467b67f76c80f6b278ccabc81dec7561e8598c
wilkowsk <104863887+wilkowsk@users.noreply.github.com>
2022-05-06 00:40:18 -0400

    Update test_array_nt.cpp

--- aifm/test/test_array_nt.cpp
+++ aifm/test/test_array_nt.cpp
@@ -1,166 +1,140 @@
 extern "C" {
 #include <runtime/runtime.h>
-#include <runtime/timer.h>
 }
 
-#include "thread.h"
-
 #include "array.hpp"
-#include "deref_scope.hpp"
 #include "device.hpp"
+#include "helpers.hpp"
 #include "manager.hpp"
 
-#include <algorithm>
-#include <chrono>
 #include <cstdint>
-#include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <memory>
-#include <unistd.h>
+#include <random>
+#include <string>
 
 using namespace far_memory;
 using namespace std;
 
-#define DEFINE_DATA_TYPE(width)                                                \
-  struct Data_##width {                                                        \
-    uint8_t buf[width];                                                        \
-  };                                                                           \
-  using Data_t = Data_##width;
+constexpr static uint64_t kCacheSize = (128ULL << 20);
+constexpr static uint64_t kFarMemSize = (4ULL << 30);
+constexpr static uint32_t kNumGCThreads = 12;
+constexpr static uint32_t kNumEntries =
+    (8ULL << 20); // So the array size is larger than the local cache size.
+constexpr static uint32_t kNumConnections = 300;
 
-DEFINE_DATA_TYPE(4096);
+uint64_t raw_array_A[kNumEntries];
 
-constexpr uint64_t kCacheSize = 1ULL << 30;
-constexpr uint64_t kFarMemSize = 8ULL << 30;
-constexpr uint64_t kArraySize = 660ULL << 20;
-constexpr uint64_t kNumGCThreads = 21;
-constexpr uint64_t kNumEntries = kArraySize / sizeof(Data_t);
-
-namespace far_memory {
-class FarMemTest {
-public:
-  void flush_core_local_regions(FarMemManager *mgr) {
-    for (int i = 0; i < helpers::kNumCPUs; i++) {
-      mgr->cache_region_manager_.try_refill_core_local_free_region(
-          0, &(mgr->cache_region_manager_.core_local_free_regions_[i]));
-      mgr->cache_region_manager_.try_refill_core_local_free_region(
-          1, &(mgr->cache_region_manager_.core_local_free_nt_regions_[i]));
-    }
+template <uint64_t N, typename T>
+void copy_array(Array<T, N> *array, T *raw_array) {
+  for (uint64_t i = 0; i < N; i++) {
+    DerefScope scope;
+    (*array).at_mut(scope, i) = raw_array[i];
   }
-
-  uint64_t read_array(Array<Data_t, kNumEntries> *array, bool nt) {
-    uint64_t num_not_present = 0;
-    for (uint64_t i = 0; i < kNumEntries; i++) {
-      DerefScope scope;
-      num_not_present += !(array->GenericArray::at(nt, i)->meta().is_present());
-      if (nt) {
-        array->at<true>(scope, i);
-      } else {
-        array->at<false>(scope, i);
-      }
-    }
-    return num_not_present;
-  }
-
-  uint64_t write_array(Array<Data_t, kNumEntries> *array, bool nt) {
-    uint64_t num_not_present = 0;
-    for (uint64_t i = 0; i < kNumEntries; i++) {
-      DerefScope scope;
-      num_not_present += !(array->GenericArray::at(nt, i)->meta().is_present());
-      if (nt) {
-        array->at_mut<true>(scope, i);
-      } else {
-        array->at_mut<false>(scope, i);
-      }
-    }
-    return num_not_present;
-  }
-
-  void clear_hot(Array<Data_t, kNumEntries> *array) {
-    for (uint64_t i = 0; i < kNumEntries; i++) {
-      auto *ptr = array->GenericArray::at(false, i);
-      if (ptr->meta().is_present()) {
-        DerefScope scope;
-        ptr->deref(scope);
-        ptr->meta().clear_hot();
-      }
-    }
-  }
-
-  void run(FarMemManager *manager) {
-    auto array_A = manager->allocate_array<Data_t, kNumEntries>();
-    auto array_B = manager->allocate_array<Data_t, kNumEntries>();
-    auto array_C = manager->allocate_array<Data_t, kNumEntries>();
-
-    // Make cache clean.
-    read_array(&array_A, false);
-    read_array(&array_B, false);
-    flush_core_local_regions(manager);
-    read_array(&array_C, false);
-    read_array(&array_A, false);
-    read_array(&array_B, false);
-
-    // Do non-temporal read.
-    read_array(&array_C, true);
-    read_array(&array_A, true);
-
-    // Check.
-    array_B.disable_prefetch();
-    auto num_not_present = read_array(&array_B, false);
-    if (num_not_present) {
-      goto fail;
-    }
-
-    // Prevent CLOCK from working.
-    clear_hot(&array_A);
-    clear_hot(&array_B);
-    clear_hot(&array_C);
-    // Make cache clean.
-    flush_core_local_regions(manager);
-    read_array(&array_C, false);
-    read_array(&array_A, false);
-    read_array(&array_B, false);
-
-    // Do non-temporal write.
-    write_array(&array_C, true);
-    write_array(&array_A, true);
-
-    // Check.
-    num_not_present = read_array(&array_B, false);
-    if (num_not_present) {
-      goto fail;
-    }
-
-    std::cout << "Passed" << std::endl;
-    return;
-  fail:
-    std::cout << "Failed" << std::endl;
-    std::cout << num_not_present << std::endl;
-  }
-};
-} // namespace far_memory
-
-void do_work(FarMemManager *manager) {
-  cout << "Running " << __FILE__ "..." << endl;
-  FarMemTest test;
-  test.run(manager);
 }
 
+template <typename T, uint64_t N>
+void add_array(Array<T, N> *array_C, Array<T, N> *array_A,
+               Array<T, N> *array_B) {
+  for (uint64_t i = 0; i < N; i++) {
+    DerefScope scope;
+    (*array_C).at_mut(scope, i) =
+        (*array_A).at(scope, i) + (*array_B).at(scope, i);
+  }
+}
+
+template <typename T, uint64_t N>
+int search_array(Array<T, N> *array_A, uint64_t targetElement) {
+  uint64_t low = 0;
+  uint64_t high = N - 1;
+  uint64_t middle;
+  uint64_t middleElement;
+  bool foundElement = false;
+  while ((high >= low) && !foundElement) {
+    DerefScope scope;
+    middle = (high + low) / 2;
+    middleElement = (*array_A).at(scope, middle);
+    if (middleElement > targetElement) { // middleElement is too high
+      high = middle - 1;
+    } else if (middleElement < targetElement) { // middleElement is too low
+      low = middle + 1;
+    } else {
+      foundElement = true;
+    }
+  }
+  if (foundElement) {
+    return middle;
+  }
+  return -1;
+}
+
+void gen_random_array(uint64_t num_entries, uint64_t *raw_array) {
+  std::random_device rd;
+  std::mt19937_64 eng(rd());
+  std::uniform_int_distribution<uint64_t> distr(2,257);
+  uint64_t value = 0;
+  for (uint64_t i = 0; i < num_entries; i++) {
+    value += distr(eng);
+    raw_array[i] = value;
+  }
+}
+
+void do_work(FarMemManager *manager) {
+  auto array_A = manager->allocate_array<uint64_t, kNumEntries>();
+
+  gen_random_array(kNumEntries, raw_array_A);
+  copy_array(&array_A, raw_array_A);
+  // add_array(&array_C, &array_A, &array_B);
+  uint64_t result;
+
+  for (uint64_t i = 0; i < 10000; i += 100) {
+    DerefScope scope;
+    result = search_array(&array_A, raw_array_A[i]);
+    if (result != i) {
+      goto fail;
+    }
+    result = search_array(&array_A, raw_array_A[i] + 1);
+    if (result != -1ULL) {
+      goto fail;
+    }
+  }
+
+  cout << "Passed" << endl;
+  return;
+
+fail:
+  cout << "Failed" << endl;
+}
+
+int argc;
 void _main(void *arg) {
+  char **argv = static_cast<char **>(arg);
+  std::string ip_addr_port(argv[1]);
+  auto raddr = helpers::str_to_netaddr(ip_addr_port);
   std::unique_ptr<FarMemManager> manager =
       std::unique_ptr<FarMemManager>(FarMemManagerFactory::build(
-          kCacheSize, kNumGCThreads, new FakeDevice(kFarMemSize)));
+          kCacheSize, kNumGCThreads,
+          new TCPDevice(raddr, kNumConnections, kFarMemSize)));
   do_work(manager.get());
 }
 
-int main(int argc, char *argv[]) {
+int main(int _argc, char *argv[]) {
   int ret;
 
-  if (argc < 2) {
-    std::cerr << "usage: [cfg_file]" << std::endl;
+  if (_argc < 3) {
+    std::cerr << "usage: [cfg_file] [ip_addr:port]" << std::endl;
     return -EINVAL;
   }
 
-  ret = runtime_init(argv[1], _main, NULL);
+  char conf_path[strlen(argv[1]) + 1];
+  strcpy(conf_path, argv[1]);
+  for (int i = 2; i < _argc; i++) {
+    argv[i - 1] = argv[i];
+  }
+  argc = _argc - 1;
+
+  ret = runtime_init(conf_path, _main, argv);
   if (ret) {
     std::cerr << "failed to start runtime" << std::endl;
     return ret;
