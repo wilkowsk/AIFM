e056ad5d14e42c9865bc8e2f8bb192e3f57d6ac4
wilkowsk <104863887+wilkowsk@users.noreply.github.com>
2022-05-14 14:19:26 -0400

    Update run.sh

--- aifm/bfs/run.sh
+++ aifm/bfs/run.sh
@@ -1,2 +1,2 @@
-./bfs_up_down_sequential /tmp/15418/random_20m.graph
-./bfs_up_down_sequential /tmp/15418/random_50m.graph
+./bfs_up_down_sequential ./tmp/15418/random_20m.graph
+./bfs_up_down_sequential ./tmp/15418/random_50m.graph
